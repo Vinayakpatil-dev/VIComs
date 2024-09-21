dbe79c6a9cc6acd921e1236cf7c16389a2b76070
tsuko07 <abijith0707@gmail.com>
2024-09-21 21:54:09 +0530

    Delete VIComs directory

--- VIComs/mess_demo.py
+++
@@ -1,7 +0,0 @@
-import smtplib
-s=smtplib.SMTP("smtp.gmail.com",587)
-s.starttls()
-s.login("abijith0707@gmail.com","kgth rtlx wqhy dtsm")
-message="hello team "
-s.sendmail("abijith0707@gmail.com","abijith0707@gmail.com",message)
-s.quit()
--- VIComs/static/Js/open.js
+++
@@ -1,6 +0,0 @@
-let animate = gsap.timeline();
-
-animate.from('.intro',{
-    y:-100,
-    duration:1,
- })
